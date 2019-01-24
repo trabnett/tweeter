3d863ca948e0aa6f9606dbe2480dfb6168dcc3e7
trabnett <timrabnett@gmail.com>
2019-01-24 21:35:40 +0000

    tweeter with mongo database added. fixing linting and presentation before submitting

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,44 +3,17 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-    },
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
-
-
-
 $(document).ready(function() {
 
+  function escape(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
 
-// $('.tweet').mouseenter(function() {
-//     $(this).find('img').attr('src', pic)
-//     $(this).find('header').find('p').html("hello world")
-//     console.log($(this), 'hello')
-//   })
 
-function escape(str) {
-  var div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-}
-
-var words = tweetData['content']['text']
-function createTweetElement(data) {
-$('#mainTweetBody').prepend(`
+  function createTweetElement(data) {
+    $('#mainTweetBody').prepend(`
       <section class="tweet">
         <article>
           <header>
@@ -52,64 +25,62 @@ $('#mainTweetBody').prepend(`
           </footer>
         </article>
       </section>`)
-  let $tweet = $('<article>').addClass('tweet');
-};
+      let $tweet = $('<article>').addClass('tweet');
+  };
 
 
-$( "form" ).submit(function( event ) {
-  $('.noChar').hide()
-  $('.tooMany').hide()
-  event.preventDefault();
-  let stringInput = $('form').serialize()
-  console.log(stringInput)
-  if (stringInput.length > 5 && stringInput.length <= 145) {
-    $.ajax('/tweets', {data: stringInput, method: 'POST' })
-    .done((response) => {
-      loadTweets()
-    })
-    .fail(() => {
-      console.log('The call failed')
-    })
-  } else if (stringInput.length <= 5){
-    $('.noChar').slideToggle(400, function () {
+  $( "form" ).submit(function( event ) {
+    $('.noChar').hide()
+    $('.tooMany').hide()
+    event.preventDefault();
+    let stringInput = $('form').serialize()
+    if (stringInput.length > 5 && stringInput.length <= 145) {
+      $.ajax('/tweets', {data: stringInput, method: 'POST' })
+      .done((response) => {
+        console.log('posting new tweet from app.js--check')
+        loadTweets()
+      })
+      .fail(() => {
+        console.log('The call failed')
+      })
+    } else if (stringInput.length <= 5){
+      $('.noChar').slideToggle(400, function () {
+        $('this, textarea').select()
+      });
+    } else {
+      $('.tooMany').slideToggle(400, function () {
+        $('this, textarea').select()
+      });
+    }
+    $('textarea').val('')
+  })
+
+
+  function loadTweets () {
+    console.log('app.js load tweets-check')
+    $('#mainTweetBody').empty()
+    $.getJSON("/tweets", function(result){
+      $.each(result, function(i, field){
+        console.log("prepending-check")
+        $("body").prepend(createTweetElement(field));
+      });
+    });
+  };
+
+  $("#nav-bar button").on('click', function() {
+    $('.new-tweet').slideToggle(400, function () {
     $('this, textarea').select()
     });
-  } else {
-    $('.tooMany').slideToggle(400, function () {
-    $('this, textarea').select()
-    });
-  }
-  $('textarea').val('')
-})
-
-
-function loadTweets () {
-  $('#mainTweetBody').empty()
-  $.getJSON("/tweets", function(result){
-    $.each(result, function(i, field){
-      $("body").prepend(createTweetElement(field));
-    });
   });
-};
-
-$("#nav-bar button").on('click', function() {
-  $('.new-tweet').slideToggle(400, function () {
-  $('this, textarea').select()
-  });
-});
 
 
 
 loadTweets()
 
 
-var $tweet = createTweetElement(tweetData);
+
+
 
 // Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-
-
-
-
-
+ // to see what it looks like
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,15 @@
-var hello = 'what'
-
-
 $(document).ready(function() {
-  console.log(hello)
+
 
 
 $("form textarea").on('keyup', function() {
-    let currentLength = $(this).val().length;
-    let charactersLeft = 140 - currentLength;
-    if (charactersLeft < 0) {
-      $(this).siblings(".counter").text(charactersLeft).css('color', 'red');
-    } else {
-        $(this).siblings(".counter").text(charactersLeft).css('color', 'black');
-    }
+  let currentLength = $(this).val().length;
+  let charactersLeft = 140 - currentLength;
+  if (charactersLeft < 0) {
+    $(this).siblings(".counter").text(charactersLeft).css('color', 'red');
+  } else {
+      $(this).siblings(".counter").text(charactersLeft).css('color', 'black');
+  }
 });
 
 
